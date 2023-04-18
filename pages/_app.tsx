f173462d6527f4390e761754d5c2189f1c77b284
yash <yashnahata78@gmail.com>
2023-04-18 11:06:16 +0530

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { Toaster } from 'react-hot-toast';
+import { SessionProvider } from 'next-auth/react';
+import type { AppProps } from 'next/app'
+import Layout from '@/components/layout'
+import '@/styles/globals.css'
+import LoginModal from '@/components/modals/LoginModal'
+import RegisterModal from '@/components/modals/RegisterModal'
+import EditModal from '@/components/modals/EditModal'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return(
+    <SessionProvider session={pageProps.session}>
+      <Toaster />
+      <EditModal />
+    <RegisterModal />
+   <LoginModal />
+    <Layout>
+   <Component {...pageProps} />
+   </Layout>
+   </SessionProvider>
+  )
+}
